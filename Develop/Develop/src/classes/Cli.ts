a5819d69cddefbd4b6642553f392daa2cf19b9a2
naiknetra <naiknetra5@gmail.com>
2024-10-01 21:13:17 -0400

    updated typescript cli.ts

--- Develop/Develop/src/classes/Cli.ts
+++ Develop/Develop/src/classes/Cli.ts
@@ -15,8 +15,8 @@ class Cli {
   exit: boolean = false;
 
   // TODO: Update the constructor to accept Truck and Motorbike objects as well
-  constructor(vehicles: (Car)[]) {
-    this.vehicles = vehicles;
+  constructor(vehicles: (Car | Truck | Motorbike)[]) {
+    this.vehicles = vehicles || [];
   }
 
   // static method to generate a vin
@@ -69,6 +69,14 @@ class Cli {
           // create a car
           this.createCar();
         }
+        else if (answers.vehicletype === 'Truck') {
+          //create a Truck
+          this.createTruck();
+      }
+      else if (answers.vehicletype === 'Motorbike') {
+          //create a Motorbike
+          this.createMotorbike();
+      }
         // TODO: add statements to create a truck or motorbike if the user selects the respective vehicle type
       });
   }
@@ -170,10 +178,18 @@ class Cli {
         },
       ])
       .then((answers) => {
+        const truck = new Truck(
         // TODO: Use the answers object to pass the required properties to the Truck constructor
         // TODO: push the truck to the vehicles array
         // TODO: set the selectedVehicleVin to the vin of the truck
         // TODO: perform actions on the truck
+        Cli.generateVin(), answers.color, answers.make, answers.model, parseInt(answers.year), parseInt(answers.weight), parseInt(answers.topSpeed), parseInt(answers.towingCapacity),[]);
+                    // push the car to the vehicles array
+                    this.vehicles.push(truck);
+                    // set the selectedVehicleVin to the vin of the car
+                    this.selectedVehicleVin = truck.vin;
+                    // perform actions on the car
+                    this.performActions();
       });
   }
 
@@ -233,10 +249,18 @@ class Cli {
         },
       ])
       .then((answers) => {
+        const motorbike = new Motorbike(
         // TODO: Use the answers object to pass the required properties to the Motorbike constructor
         // TODO: push the motorbike to the vehicles array
         // TODO: set the selectedVehicleVin to the vin of the motorbike
         // TODO: perform actions on the motorbike
+        Cli.generateVin(), answers.color, answers.make, answers.model, parseInt(answers.year), parseInt(answers.weight), parseInt(answers.topSpeed), parseInt(answers.frontWheelDiamater), parseInt(answers.frontWheelBrand), parseInt(answers.rearWheelDiameter), parseInt(answers.rearWheelBrand), []);
+        // push the car to the vehicles array
+        this.vehicles.push(motorbike);
+        // set the selectedVehicleVin to the vin of the car
+        this.selectedVehicleVin = motorbike.vin;
+        // perform actions on the car
+        this.performActions();
       });
   }
 
